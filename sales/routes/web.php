e84c9602c9228fdf092b4b573edafa06d809e09f
noyonali492 <noyonahmed492@gmail.com>
2024-07-05 20:50:48 +0600

    group route & Prefix Route

--- sales/routes/web.php
+++ sales/routes/web.php
@@ -7,6 +7,27 @@ Route::get('/', function () {
 Route::get('about/', function () {
     return view('about');
 });
+
+// name Route
 Route::get('posttt/', function () {
     return view('post');
 })->name('post');
+
+
+// route group .. Prefix Route
+route::prefix('page')->group(function(){
+
+            route::get('/gellary', function(){
+                    return "<h1>Gellary Page</h1>";
+            });
+
+            route::get('/vedio', function(){
+                return "<h1>vedio Page</h1>";
+              });
+
+        route::get('/audio', function(){
+            return "<h1>audio Page</h1>";
+         });
+
+
+});

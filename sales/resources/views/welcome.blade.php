9824ed308bebd1cf8d8b8a68a05e96d96025275e
noyonali492 <noyonahmed492@gmail.com>
2024-07-05 22:49:30 +0600

    Laravel Blade Template Basis work

--- sales/resources/views/welcome.blade.php
+++ sales/resources/views/welcome.blade.php
@@ -6,3 +6,18 @@
 
 <a href="{{route('post')}}">Post</a> </br>
 <a href="about">About</a>
+
+</br>
+{{ "Noyon ali" }}
+</br>
+
+{!! "<h1>HTML Tag Modde Print </h1>" !!}
+
+
+@php
+    $name =['ALim','khalim','abul'];
+@endphp
+@foreach ($name as $item)
+</br>
+    {{ $item}}
+@endforeach

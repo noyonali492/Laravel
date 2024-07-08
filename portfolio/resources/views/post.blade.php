e0526377b0b23a6a9b74ff6947b8618e85dfc21e
noyonali492 <noyonahmed492@gmail.com>
2024-07-08 23:00:24 +0600

    blade tamplate Variable er jonnosudu @php Declear korte hoi.

--- portfolio/resources/views/post.blade.php
+++ portfolio/resources/views/post.blade.php
@@ -1,4 +1,4 @@
-<h1>Post page<h1>
+<h1>Post page<php>
 
 
 
@@ -9,9 +9,23 @@
     {{$name}}
 
     @if ($id==1)
-     <h1>Admin Active</h1>
+     <h3>Admin Active</h3>
      @elseif($name == 'noyon')
      <h3>Lowest Admin Active</h3>
     @else
     <h1 style="color: red">Admin Not Active</h1>
     @endif
+
+@php
+    $a=5;
+    $b=7;
+    $sum=$a+$b; 
+@endphp
+    @if($sum>=8)
+    <h4>Largest{{$sum}}</h4>
+    @else
+        <h3>Lowest</h3>
+    @endif
+    {{$sum}}
+
+    

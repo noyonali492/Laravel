ffe5921140b9bf109fcc24dbd073d008f6fba581
noyonali492 <noyonahmed492@gmail.com>
2024-07-09 20:03:32 +0600

    Arithmetic Operators

--- sales/resources/views/Operator.blade.php
+++ sales/resources/views/Operator.blade.php
@@ -11,9 +11,9 @@
  A={{ $a }}<br>
  B={{ $b }}
 
-<h4>For A+B, The Result is = {{$a+$b}}</h4>
-<h4>For A-B, The Result is = {{$a-$b}}</h4>
-<h4>For A*B, The Result is = {{$a*$b}}</h4>
-<h4>For A/B, The Result is = {{$a/$b}}</h4>
-<h4>For A%B, The Result is = {{$a%$b}}</h4>
-<h4>For A**B, The Result is = {{$a**$b}}</h4>
+<h4>Addition, For A+B, The Result is = {{$a+$b}}</h4>
+<h4>Subtraction, For A-B, The Result is = {{$a-$b}}</h4>
+<h4>Multiplication, For A*B, The Result is = {{$a*$b}}</h4>
+<h4>Division, For A/B, The Result is = {{$a/$b}}</h4>
+<h4>Modulus, For A%B, The Result is = {{$a%$b}}</h4>
+<h4>Exponentiation, For A**B, The Result is = {{$a**$b}}</h4>

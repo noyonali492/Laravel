fdbd442bfd6d6a3479ef749834736a534cb5d0bd
noyonali492 <noyonahmed492@gmail.com>
2024-07-09 20:21:13 +0600

    PHP Assignment Operators

--- sales/resources/views/Operator.blade.php
+++ sales/resources/views/Operator.blade.php
@@ -5,11 +5,16 @@
  @php
     $a=8;
     $b=2;  
+    $x=8;
+    $y=10;
+    $z=12;
  @endphp
 
+A={{ $a }}<br>
+B={{ $b }}
+
+
 <h1>Arithmetic operators</h1> 
- A={{ $a }}<br>
- B={{ $b }}
 
 <h4>Addition, For A+B, The Result is = {{$a+$b}}</h4>
 <h4>Subtraction, For A-B, The Result is = {{$a-$b}}</h4>
@@ -17,3 +22,21 @@
 <h4>Division, For A/B, The Result is = {{$a/$b}}</h4>
 <h4>Modulus, For A%B, The Result is = {{$a%$b}}</h4>
 <h4>Exponentiation, For A**B, The Result is = {{$a**$b}}</h4>
+
+
+<h1>PHP Assignment Operators</h1>
+x = y	x = y	The left operand gets set to the value of the expression on the right	<br>
+x += y	x = x + y	Addition	<br>
+x -= y	x = x - y	Subtraction	<br>
+x *= y	x = x * y	Multiplication	<br>
+x /= y	x = x / y	Division	<br>
+x %= y	x = x % y	Modulus<br>
+
+<h4>{{ $a }}</h4>
+
+x += y {{ $a += 100}} <br>
+x -= y {{ $b -= 1}}<br>
+
+x *= y {{$x *= 6}}<br>
+x -= y {{$y /= 5}}<br>
+x %= y {{$y %= 5}}

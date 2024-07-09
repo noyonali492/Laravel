642a80e433537d1feca6048cedf5df5900822c49
noyonali492 <noyonahmed492@gmail.com>
2024-07-09 20:00:15 +0600

    Arithmetic operators

--- sales/resources/views/Operator.blade.php
+++ sales/resources/views/Operator.blade.php
@@ -8,6 +8,12 @@
  @endphp
 
 <h1>Arithmetic operators</h1> 
-
+ A={{ $a }}<br>
+ B={{ $b }}
 
 <h4>For A+B, The Result is = {{$a+$b}}</h4>
+<h4>For A-B, The Result is = {{$a-$b}}</h4>
+<h4>For A*B, The Result is = {{$a*$b}}</h4>
+<h4>For A/B, The Result is = {{$a/$b}}</h4>
+<h4>For A%B, The Result is = {{$a%$b}}</h4>
+<h4>For A**B, The Result is = {{$a**$b}}</h4>

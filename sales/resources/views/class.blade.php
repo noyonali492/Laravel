d5ed68302b4ada78e5b31eabd8b3b14ebf8df555
noyonali492 <noyonahmed492@gmail.com>
2024-07-15 22:28:29 +0600

    Public

--- sales/resources/views/class.blade.php
+++ sales/resources/views/class.blade.php
@@ -1,1 +1,35 @@
 <h1>Class Practice</h1>
+
+@php
+
+// class Stdinfo{ 
+//    public $name="Shobuz";
+//    public function Mathsubject(){
+//     echo $this->name;
+
+//    }
+// }
+
+//     $mathinfo= new Stdinfo();
+//     $mathinfo->Mathsubject();
+//     //echo "name". $mathinfo->name;
+
+
+
+
+    class techerinfo{ 
+
+    public $math_techer_name="Abdul Mannan";
+    public $camisty_ath_techer_name= "Rudro";
+        public function teinfo(){
+            echo $this->math_techer_name."<br>" ;
+            
+            echo $this->camisty_ath_techer_name;
+        }
+    
+    }
+
+    $altecherinfo = new techerinfo();
+    $altecherinfo->teinfo();
+
+@endphp

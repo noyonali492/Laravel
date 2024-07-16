d3a4c9bdaeec04049a4b071050b5c366d40589fe
noyonali492 <noyonahmed492@gmail.com>
2024-07-16 20:54:07 +0600

    extends methode & childe class property add

--- sales/resources/views/Inheritance.blade.php
+++ sales/resources/views/Inheritance.blade.php
@@ -14,20 +14,35 @@
         }
 
         public function info(){
-            echo "Employe Informaion <br>";
+            echo " <h1> Employe Informaion </h1><br>";
             echo " Employe Name : ".$this->name. "<br>";
             echo " Employe age : ".$this->age. "<br>";
-            echo " Employe Salary :".$this->salary;
+            echo " Employe Salary :".$this->salary. "<br> <br> <br>";
         
         }
     }
 
     class manager extends employe{
+        public $ta= 2500;
+        public $mobile =1200;
+        public $totalsalary;
+        
+        public function info(){
+            $this->totalsalary = $this->ta + $this->mobile + $this->salary;
 
+            echo " <h2> Manager Informaion </h2><br>";
+            echo " Manager Name : ".$this->name. "<br>";
+            echo " Manager age : ".$this->age. "<br>";
+            echo " Manager Salary :".$this->totalsalary;
+        
+        }
     }
 
-    $el= new manager("noyon",27,25000);
+    $el= new employe("naim",27,25000);
     $el->info();
+
+    $el1= new manager('Noyon',27,27000);
+    $el1->info();
 
     
 @endphp

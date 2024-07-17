6711c93cd43c72fc20c826a77bfb1aed91b16cc2
noyonali492 <noyonahmed492@gmail.com>
2024-07-17 21:43:39 +0600

    constant class er modde print

--- sales/resources/views/constantclass.blade.php
+++ sales/resources/views/constantclass.blade.php
@@ -3,10 +3,23 @@
 @php
     
 
-class Goodbye {
-    const LEAVING_MESSAGE = "Thank you for visiting W3Schools.com!";
-  }
+// class Goodbye {
+//     const LEAVING_MESSAGE = "Thank you for visiting noyonahmed.com";
+//   }
   
-  echo Goodbye::LEAVING_MESSAGE;
+//   echo Goodbye::LEAVING_MESSAGE;
+
+
+
+
+  class Goodbye {
+  const LEAVING_MESSAGE = "Thank you for visiting W3Schools.com!";
+  public function byebye() {
+    echo self::LEAVING_MESSAGE;
+  }
+}
+
+$goodbye = new Goodbye();
+$goodbye->byebye();
 
   @endphp

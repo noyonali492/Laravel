191eefa2d242a02e13188611e07d5d151758950a
noyonali492 <noyonahmed492@gmail.com>
2024-08-04 23:01:44 +0600

    new

---
+++ portfolio/namespace/index.php
@@ -0,0 +1,13 @@
+<?php
+require 'product.php';
+require 'test.php';
+
+$obj = new pro\ product();
+$obj = new test\ product();
+
+function wow(){
+    echo "noyon index";
+}
+
+pro\wow();
+?>
---
+++ portfolio/namespace/product.php
@@ -0,0 +1,14 @@
+<?php
+ namespace pro;
+class product{
+    public function __construct(){
+       // echo " This Is Product Page <br>";
+       $obj = new \test\ product();
+    }
+}
+
+
+function wow(){
+    echo "noyon product";
+}
+?>
